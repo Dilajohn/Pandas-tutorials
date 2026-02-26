2456bb01cc19144c1dbce05a283d360f70f853cb
Okuja Emmanuel <okujaemmanuel97@gmail.com>
2026-02-26 12:28:04 +0300

    Set index column when reading wine reviews CSV file

--- data_analysis.py
+++ data_analysis.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
-wines_reviews = pd.read_csv("winemag-data-130k-v2.csv")
+wines_reviews = pd.read_csv("winemag-data-130k-v2.csv", index_col=0)
+
+
+#print(wines_reviews.head())
+print(wines_reviews)
 
 
