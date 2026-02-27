99f13936213300cb7e68b5dad6432412f874a622
Okuja Emmanuel <okujaemmanuel97@gmail.com>
2026-02-27 09:26:16 +0300

    add initial implementation for loading and accessing wine reviews data

---
+++ indexing_selecting_and_Assigning.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+reviews = pd.read_csv("winemag-data-130k-v2.csv", index_col=0)   # Load the CSV file into a DataFrame,
+#print(reviews.head()) # Print the first few rows of the DataFrame to get an overview of the data
+
+pd.set_option("display.max_rows", 5)  # Set the maximum number of rows to display to 5
+#print(reviews)  # Print the DataFrame to see the effect of the new display option
+
+#---- ACCESSING THE PROPERTY OF A DATAFRAME ---
+
+print(reviews.country)  # Access the 'country' column of the DataFrame, which returns a Series
+print(reviews["country"])  # Access the 'country' column using bracket notation, which also returns a Series
