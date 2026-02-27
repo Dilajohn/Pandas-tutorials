c3dc3814028a598295cda7cb2c2139eaa15495ef
Okuja Emmanuel <okujaemmanuel97@gmail.com>
2026-02-27 07:00:54 +0300

    add cooments to the code snnipet

--- data_analysis.py
+++ data_analysis.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
-wines_reviews = pd.read_csv("winemag-data-130k-v2.csv", index_col=0)
+wines_reviews = pd.read_csv("winemag-data-130k-v2.csv", index_col=0)   # Load the CSV file into a DataFrame,
+                                                                       #using the first column as the index
 
-#print(wines_reviews.head())
-print(wines_reviews)
+print(wines_reviews.head())  # Print the first few rows of the DataFrame to get an overview of the data
+#print(wines_reviews)
 
 
