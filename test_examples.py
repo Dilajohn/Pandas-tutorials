f6425061949d4e5f4e2ee2ea99a4ce1a71473035
Okuja Emmanuel <okujaemmanuel97@gmail.com>
2026-03-04 10:13:32 +0300

    Fix variable name and update print statement to display DataFrame info

--- test_examples.py
+++ test_examples.py
@@ -1,7 +1,13 @@
 import pandas as pd
 
-wines_reviews = pd.read_csv("winemag-data-130k-v2.csv", index_col=0)   # Load the CSV file into a DataFrame,
+reviews = pd.read_csv("winemag-data-130k-v2.csv", index_col=0)   # Load the CSV file into a DataFrame,
 
 #Look at an overview of your data by running the following line
-print(wines_reviews.info)  # Print a concise summary of the DataFrame,
+print(reviews.info)  # Print a concise summary of the DataFrame,
                            #including the number of non-null entries and data types of each column
+                           
+#Select the description column from reviews and assign the result to the variable desc.
+print(reviews.description)
+print(reviews['description'])
+desc = reviews['description']
+print(type(desc))
